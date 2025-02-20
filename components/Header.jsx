9e134f3e663a7452981dc43088a272f46528ea7f
Amandamecchi <amandamechi327@gmail.com>
2025-02-20 11:53:34 -0300

    Correções

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function header(){
+    return (
+       <header className={styles.header}>
+<h1 className={styles.title}>CareBear</h1>
+       </header> 
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,23 @@
 import styles from "../styles/Home.module.css"
-//import BlueBear from "../public/"
+import header from "../components/Header";
 
 export default function Home(){
     return (
         <div className={styles.container}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>2TDS1</h1>
+            <header>
+                <div className={styles.bloco}>
+                <h1>BadTime</h1>
+                <img className ={styles.urso} src="/download.png" alt="download.png" />
+
+                </div>
             </header>
-<img src="BlueBear" alt="blueBear" />
+            <main className={styles.main}>
+            </main>
+
+            <div className={styles.footer}>
+                <h3>amanda</h3>
+            </div>
         </div>
+        
     );
 }
