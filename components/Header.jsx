389f41070490a707d1444f7664fb5a4b323bf91b
Amandamecchi <amandamechi327@gmail.com>
2025-02-20 14:07:03 -0300

    Correções

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,12 @@ import styles from '../styles/Home.module.css'
 
 export default function header(){
     return (
-       <header className={styles.header}>
-<h1 className={styles.title}>CareBear</h1>
-       </header> 
+        <div className={styles.container}>
+        <div className={styles.header}>
+            <h1>BadTime</h1>
+            <img className={styles.urso} src="/download.png" alt="download.png" />
+        </div>
+        </div>
+
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,37 @@
 import styles from "../styles/Home.module.css"
-import header from "../components/Header";
+import Header from "../components/Header";
 
-export default function Home(){
+export default function Home() {
     return (
         <div className={styles.container}>
-            <header>
-                <div className={styles.bloco}>
-                <h1>BadTime</h1>
-                <img className ={styles.urso} src="/download.png" alt="download.png" />
-
-                </div>
-            </header>
+<Header />
             <main className={styles.main}>
+
             </main>
 
+            <div className={styles.article}>
+                <div className={styles.box1}>
+                    <h1>1</h1>
+                </div>
+
+                <div className={styles.box2}>
+                    <h1>2</h1>
+                </div>
+
+                <div className={styles.box3}>
+                    <h1>3</h1>
+                </div>
+
+            </div>
+
             <div className={styles.footer}>
-                <h3>amanda</h3>
+                <h3>footer</h3>
+            </div>
+
+            <div className={styles.imagem}>
+            <img className={styles.cloud} src="/CloudBear.jpg" alt="urso" />
             </div>
         </div>
-        
+
     );
 }
