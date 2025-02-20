d534d4c36a7c3b3739a1eeead111e2ab7458a239
Amandamecchi <amandamechi327@gmail.com>
2025-02-20 10:23:07 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css"
+//import BlueBear from "../public/"
+
+export default function Home(){
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1 className={styles.title}>2TDS1</h1>
+            </header>
+<img src="BlueBear" alt="blueBear" />
+        </div>
+    );
+}
